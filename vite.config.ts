0d225e9ab650ce3fe74d2d27f037db5c691254d3
ARTEMISFOWL-01 <148075903+ARTEMISFOWL-01@users.noreply.github.com>
2025-06-08 11:39:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "development" ? "/" : process.env.VITE_BASE_PATH || "/",
+ base: "/PORTFOLIO_WEBPAGE/",
+
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
